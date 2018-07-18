3f73cdd8a7bce2b0cdb93aaa4d4d7f6f52026c84
Jamiu Oloyede <oloyedejamiu@gmail.com>
2018-07-18 11:34:45 +0100

    Learn __get & __set

--- oop_basic_eduinox/__get__set.php
+++ oop_basic_eduinox/__get__set.php
@@ -1,2 +1,24 @@
 <?php 
+
 // Learning __get & __set methods
+class Post {
+	private $name;
+	public function __set($name, $value){
+		echo 'Setting '. $name . ' to <strong>' . $value . '</strong> <br>';
+		$this->name = $value;
+	}
+
+	public function __get($name){
+		echo 'Getting '. $name . '<strong>' . $this->name . '</strong> <br>';
+	}
+
+	public function __isset($name){
+		echo 'Is ' . $name . ' set? <br>';
+		return isset($this->name);
+	}
+}
+
+$post = new Post;
+$post->name = 'testing';
+echo $post->name;
+var_dump(isset($post->name));
